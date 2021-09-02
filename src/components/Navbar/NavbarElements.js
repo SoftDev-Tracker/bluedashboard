91f60899394ae156737a7fc8bcf5cbe645880ac4
iCharlieAraiza <eliptikman1234@gmail.com>
2021-09-02 04:45:56 -0500

    First commit

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+    background-color: #ffffff;
+    display: flex;
+    height: 80px;
+    justify-content: space-between;
+    position: fixed;
+    width: 100%;
+`
+export const NavbarContainer = styled.
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Nav } from './NavbarElements'
+import logo from '../../assets/img/logov1.png'
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo>
+
+                    </NavLogo>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
