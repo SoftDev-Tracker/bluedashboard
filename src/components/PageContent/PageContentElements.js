0a61a4e55b5040aa8f1e8cb63a63058f1e830e53
iCharlieAraiza <eliptikman1234@gmail.com>
2021-09-08 06:40:23 -0500

    Portal first page

--- src/components/PageContent/PageContentElements.js
+++ src/components/PageContent/PageContentElements.js
@@ -5,6 +5,7 @@ export const MainSection = styled.div`
     min-height: 100vh;
     overflow: hidden;
     padding-top: 3.3rem;
+    box-sizing: border-box;
 `
 
 export const PageContainer = styled.main`
@@ -12,10 +13,7 @@ export const PageContainer = styled.main`
     box-sizing: border-box;
     margin-right: auto;
     margin-left: auto;
-    min-height: 100vh;
     padding: 1rem;
-
-
 
     @media only screen and (min-width: 768px){
         margin-left: 250px;
